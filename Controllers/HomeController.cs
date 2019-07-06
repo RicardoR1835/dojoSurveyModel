c62fb6da637090e57974966afc160b300115c5ff
ricardor1835 <rick.rivera1835@gmail.com>
2019-07-06 07:59:15 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dojosurv.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace dojosurv.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create(MyModel user)
+        {
+            
+            return View("Show", user);
+        }
+
+        
+
+    }
+}
---
+++ Models/MyModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace dojosurv.Models
+{
+    public class MyModel
+    {
+       public string Name {get;set;}
+       public string Email {get;set;}
+       public string Location {get;set;}
+       public string Language {get;set;}
+       public string Comment {get;set;}
+    }
+}
